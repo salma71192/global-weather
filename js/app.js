f8e1d64eaaca34c6573fbf6c9cd7c734a97c2faf
Salma Mohammed <sm.71175@gmail.com>
2018-01-31 15:54:27 +0200

    tempreture and weather condition

--- js/app.js
+++ js/app.js
@@ -17,8 +17,12 @@ $(document).ready(function() {
     $.ajax({
       url: urlString,
       success: function (result) {
-        $(".city").text(result.name + ", ");
-        $(".country").text(result.sys.country);
+        $("#city").text(result.name + ", ");
+        $("#country").text(result.sys.country);
+        currentTempInCelsius = Math.round(result.main.temp * 10) / 10;
+        $("#temp").text(currentTempInCelsius + " " + String.fromCharCode(176));
+        $("#tempunit").text("C");
+        $("#wc").text(result.weather[0].main);
       }
     });
   }
