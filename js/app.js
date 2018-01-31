2d97e818092088363ad629b6611b18cf68b8dd59
Salma Mohammed <sm.71175@gmail.com>
2018-01-31 22:32:13 +0200

    modifications

--- js/app.js
+++ js/app.js
@@ -1,6 +1,3 @@
-import { googleMap } from './googleMap';
-import { getWeather } from './getWeather';
-
 let api = "https://fcc-weather-api.glitch.me/api/current?";
 
 $(document).ready(function() {
@@ -14,6 +11,45 @@ $(document).ready(function() {
           message.innerHTML = "Geolocation is not supported by this browser.";
       }
   }
+
+  // getWeather function to upload weather information
+  function getWeather(lt, ln) {
+    var urlString = api + lt + "&" + ln;
+    $.ajax({
+      url: urlString,
+      success: function (result) {
+        $("#city").text(result.name + ", ");
+        $("#country").text(result.sys.country);
+
+        $("#temp").text(currentTempInCelsius + " " + String.fromCharCode(176));
+        $("#tempunit").text("C");
+        $("#wc").text(result.weather[0].main);
+      }
+    });
+  }
+
+  // Google map function
+  function googleMap(lt, ln) {
+    var latlon = new google.maps.LatLng(lt, ln);
+    var mapholder = document.getElementById('mapholder');
+    mapholder.classList.add('mapholder');
+
+    var myOptions = {
+      center:latlon,
+      zoom:14,
+      mapTypeId:google.maps.MapTypeId.ROADMAP,
+      mapTypeControl:false,
+      navigationControlOptions:{style:google.maps.NavigationControlStyle.SMALL}
+    }
+
+    var map = new google.maps.Map(mapholder, myOptions);
+    var marker = new google.maps.Marker({
+      position:latlon,
+      map:map,
+      title: result.name
+    });
+  }
+
 
   // function to getLocation
   function showPosition(position) {
